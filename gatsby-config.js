222a8615c8afcef14efca4b27956c70cfc1bb0bf
Carlo Janea <janeacarlo@gmail.com>
2020-02-16 14:37:03 +0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog | Carlo Janea`,
+    description: `Blogs on tech, life stories, and anything I've thought upon. (Carlo Janea)`,
+    author: `@carlojanea`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,22 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    "gatsby-plugin-emotion",
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: ["DM Sans"],
+        display: "swap",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,9 +9,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      Welcome to blog
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
