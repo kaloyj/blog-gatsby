d9a431d9e9eae02d46d2532498f7a41e38a7d9ee
Carlo Janea <janeacarlo@gmail.com>
2021-06-19 17:08:34 +0800

    contextualize env locally only

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+if (process.env.NODE_ENV === "development")
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
 
 module.exports = {
   siteMetadata: {
